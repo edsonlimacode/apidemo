1988b83e0e8a2d6d6f7be967373d4cda60375350
edson <edsonlimacode@gmail.com>
2025-11-04 10:05:18 -0300

    first commit

---
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.model.Usuario;
+import com.example.demo.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<Usuario> listar(){
+        return this.usuarioRepository.findAll();
+    }
+}
---
+++ src/main/java/com/example/demo/model/Usuario.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "contato")
+    private String contato;
+}
